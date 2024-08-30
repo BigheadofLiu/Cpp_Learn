c17d41e15d3214307aae754c3cc7af0be2f8060f
BigheadofLiu <871314635@qq.com>
2024-08-30 11:41:35 +0800

    modified:   "18.STL/18.4\345\270\270\347\224\250\345\256\271\345\231\250/8.map_multimap/map.cpp" 	new file:   "18.STL/18.4\345\270\270\347\224\250\345\256\271\345\231\250/8.map_multimap/output/map.exe"

--- 18.STL/18.4常用容器/8.map_multimap/map.cpp
+++ 18.STL/18.4常用容器/8.map_multimap/map.cpp
@@ -46,16 +46,28 @@ class mycompare{
         return val1>val2;
     }
 };
-void print_map(map<int,int>& m){
+void print_map(map<int,int,mycompare>& m){
+    for(map<int,int,mycompare>::iterator it=m.begin();it!=m.end();it++){
+        cout<<it->first<<" "<<it->second<<endl;
+    }
     
 }
 void test05(){
     map<int,int,mycompare> m1;
     for(int i=1;i<10;i++){
         for(int j=10;j<19;j++){
-            m1.insert(i,j);
+            m1.insert(make_pair(i,j));   //map只能接收pair类型的数据
         }
     }
-
-
+    // m1.insert(make_pair(10,20));
+    // m1.insert(make_pair(30,40));
+    // m1.insert(make_pair(50,60));
+    // m1.insert(make_pair(40,80));
+    // m1.insert(make_pair(90,100));
+    // m1.insert(make_pair(110,120));
+    
+    print_map(m1);
+}
+int main(){
+    test05();
 }
