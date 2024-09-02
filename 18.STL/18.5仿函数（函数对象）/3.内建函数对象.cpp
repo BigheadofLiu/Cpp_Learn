bfbdc568b545020ac69779ed4f6df44b2dae63d5
BigheadofLiu <871314635@qq.com>
2024-09-02 11:58:44 +0800

    提交

---
+++ 18.STL/18.5仿函数（函数对象）/3.内建函数对象.cpp
@@ -0,0 +1,25 @@
+/*
+STL内建的函数对象（自带）
+算术仿函数
+关系仿函数
+逻辑仿函数
+
+头文件：#include<functional>
+1.算术仿函数
+template<class T> T plus<T> //加法仿函数
+template<class T> T minus<T> //减法仿函数
+template<class T> T multiplies<T> //乘法仿函数
+template<class T> T divides<T> //除法仿函数
+template<class T> T modulus<T> //取模仿函数
+template<class T> T negate<T> //取反仿函数
+
+2.关系仿函数
+template<class T> bool equal_to<T> //等于
+template<class T> bool not_equal_to<T> //不等于
+template<class T> bool greater<T> //大于
+template<class T> bool greater_equal<T> //大于等于
+template<class T> bool less<T> //小于
+template<class T> bool less_equal<T> //小于等于
+
+*/
+
