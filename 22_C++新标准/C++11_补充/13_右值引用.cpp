e6dd436a1d3b76613974a584ac4f17467828b712
BigheadofLiu <871314635@qq.com>
2025-02-06 18:40:33 +0800

    //提交

--- 22_C++新标准/C++11_补充/13_右值引用.cpp
+++ 22_C++新标准/C++11_补充/13_右值引用.cpp
@@ -1,6 +1,6 @@
 //
-// Created by ZYB on 2025/1/8.
-//
+//  Created by ZYB on 2025/1/8.
+//  转自：https://subingwen.cn/cpp/rvalue-reference/#3-%E7%9A%84%E7%89%B9%E6%80%A7
 //  C++11 增加了一个新的类型，称为右值引用（ R-value reference），标记为 &&。在介绍右值引用类型之前先要了解什么是左值和右值：
 //  lvalue 是loactor value的缩写，rvalue 是 read value的缩写
 //  左值是指存储在内存中、有明确存储地址（可取地址）的数据；
@@ -46,8 +46,8 @@ int a1;
 A&& a4=get_obj_1(); //发生深拷贝
 //const A& a5=get_obj_1();  //可以用常量引用接收右值
 }
-//性能优化
-/*在C++中在进行对象赋值操作的时候，
+// 性能优化
+/* 在C++中在进行对象赋值操作的时候，
  * 很多情况下会发生对象之间的深拷贝，
  * 如果堆内存很大，这个拷贝的代价也就非常大，
  * 在某些情况下，如果想要避免对象的深拷贝，
@@ -72,11 +72,9 @@ B get_obj_2(){
     return  b;
 }
 void test02(){
-    B b=get_obj_2();
+    B b=get_obj_2(); //这里为什么没有调用拷贝构造函数？：deepseek：编译器应用了RVO优化？
     cout<<"b.num:"<<*b.num<<endl;
-    //construct:i`m your dad  调用了两次构造函数（效率低）
-    //construct:i`m your dad
-    //b.num:100
+
     /*右值引用具有移动语义，移动语义可以将资源（堆、
     系统对象等）通过浅拷贝从一个对象转移到另一个对
     象这样就能减少不必要的临时对象的创建、拷贝以及
@@ -84,13 +82,112 @@ void test02(){
      */
 }
 
+/*右值引用具有移动语义，移动语义可以将资源（堆、系统对象等）通过
+浅拷贝从一个对象转移到另一个对象这样就能减少不必要的临时对象的
+创建、拷贝以及销毁，可以大幅提高C++应用程序的性能。*/
+
+class C{
+public:
+    C():num(new int(100)){
+        cout<<"construct:i`m your dad"<<endl;
+    }
+    C(const B& c):num(new int(*c.num)){  //(深复制）
+        cout<<"copy construct:i`m your father"<<endl;
+    }
+    C(C&& c):num(c.num){ //移动构造函数（浅复制）
+    c.num= nullptr;
+    cout<<"move construct:i`m your dad!"<<endl;
+    }
+    ~C(){
+        delete num;
+        cout<<"destruct"<<endl;
+    }
+    int *num;
+};
+C get_obj_3(){
+    C c;
+//    return  c;
+    return std::move(c); //显式地告诉编译器将c视为右值 从而避免rvo
+}
+void test03(){
+  C&& c=get_obj_3();  //为什么这里没有发生移动构造？
+  cout<<"c.num:"<<*c.num<<std::endl;
+}
+
+/*template <class T1>
+void fun_1(T1&& param);
+
+template<class T2>
+void fun_2(const T2&& param);
+
+void test04(){
+    fun_1(10); //传入右值
+    int x=10;
+    fun_1(x); //传入左值
+    fun_2(10); //传入右值
+//    fun_2(x); //传入左值:error
+}*/
+
+/*
+ * &&的特性
+ * 如果是模板参数需要指定为T&&，如果是自动类型推导需要指定为auto &&，
+ * 在这两种场景下 &&被称作未定的引用类型。另外还有一点需要额外注意const T&&表示一个右值引用，不是未定引用类型。
+ * 未定型引用：可推导为右值&左值引用
+ * 通过右值推导 T&& 或者 auto&& 得到的是一个右值引用类型
+ * 通过非右值（右值引用、左值、左值引用、常量右值引用、常量左值引用）
+ * 推导 T&& 或者 auto&& 得到的是一个左值引用类型
+ *
+ */
+void test05(){
+    int&& x1=10; //右值引用
+    auto &&a1=x1; //右值引用推导为左值引用
+    auto &&a2=10;  //右值推导为右值引用
+    int x2=10;
+    auto&& a3=x2; //左值推导为左值引用
+}
+
+void print_vaule(int& a){
+cout<<"l vaule"<<endl;
+}
+void print_vaule(int&& a){
+cout<<"r vaule"<<endl;
+}
+/*void print_vaule(int a){
+cout<<
+}*/
+void get_vaule(int&& a){
+    print_vaule(a);
+}
+void test06(){
+    int a=10;
+    int&& b=20;
+    print_vaule(a);
+    print_vaule(b);
+    print_vaule(1314);
+    get_vaule(100);
+}
+/*
+ * 左值和右值是独立于他们的类型的，右值引用类型可能是左值也可能是右值。
+   编译器会将已命名的右值引用视为左值，将未命名的右值引用视为右值。
+   auto&&或者函数参数类型自动推导的T&&是一个未定的引用类型，它可能是左值引用也可
+   能是右值引用类型，这取决于初始化的值类型（上面有例子）。
+   通过右值推导 T&& 或者 auto&& 得到的是一个右值引用类型，其余都是左值引用类型。
+   */
 int main(){
 //    test01();
 //    构造函数：i'm tom!
 //    构造函数：i'm tom!
 
-test02();
+//test02();
 //    construct:i`m your dad
 //    b.num:100
+
+//    test03();
+//test06();
+/*  l vaule
+    l vaule
+    r vaule
+    l vaule
+*/
     return 0;
 }
---
+++ 22_C++新标准/C++11_补充/14_移动语义与完美转发.cpp
@@ -0,0 +1,72 @@
+//
+// Created by ZYB on 2025/2/6.
+//
+#include "iostream"
+#include "vector"
+using namespace std;
+// 1.move 使用std::move方法可以将左值转换为右值。
+// 使用这个函数并不能移动任何东西，而是和移动构造函数一样都具有移动语义，
+// 将对象的状态或者所有权从一个对象转移到另一个对象，只是转移，没有内存拷贝。
+// 拷贝构造--深拷贝
+// 移动构造--资源转移
+// 浅拷贝--仅复制指针
+class A{
+public:
+    A(){};
+};
+void test01(){
+    A a;
+//    A&& b=a; //error:Rvalue reference to type 'A' cannot bind to lvalue of type 'A'
+    A&& c=move(a); //success
+}
+
+void test02(){
+    vector<int> v1;
+    for (int i = 0; i < 10; ++i) {
+        v1.push_back(i);
+    }
+//    vector<int> v2=v1; //开销大（拷贝构造：深拷贝）
+    vector<int> v3= std::move(v1); //开销小：（移动构造函数） 移动之后v1还存在吗？
+    cout<<v3.size()<<endl; // 10
+    cout<<v1.size()<<endl; // 0
+    for(auto i:v3){
+        cout<<i<<endl;
+    }//0
+
+    for(auto i:v1){
+    cout<<i<<endl;
+    }//0~9
+}
+
+/*std::forward<T>(t);
+ * 使用forward用于保留参数的（左/右）值属性
+   当T为左值引用类型时，t将被转换为T类型的左值
+   当T不是左值引用类型时，t将被转换为T类型的右值
+
+   std::forward<T>(x) 用于完美转发，保持左值/右值的原始属性。
+   std::move(x) 强制变为右值，适用于不再使用的对象。
+   */
+template <class T>
+void get_vaule(T&){
+cout<<"l vaule"<<endl;
+}
+
+template <class T>
+void get_vaule(T&&){
+cout<<"r value"<<endl;
+}
+template <class T>
+void fun1(T&& t){
+    get_vaule(t);
+}
+void test03(){
+    int a=10;
+    int&& b=20;
+    fun1(1);   // l vaule
+    fun1(a);  // l vaule
+    fun1(b);  // l vaule
+}
+int main(){
+//    test02();
+test03();
+}
