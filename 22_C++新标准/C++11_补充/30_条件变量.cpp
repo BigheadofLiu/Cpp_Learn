6e985b3726c870ed1c371f225b525f2ff1109366
BigheadofLiu <871314635@qq.com>
2025-03-21 18:21:03 +0800

    提交

--- 22_C++新标准/C++11_补充/30_条件变量.cpp
+++ 22_C++新标准/C++11_补充/30_条件变量.cpp
@@ -3,8 +3,10 @@
 //
 // condition_variable
 // condition_variable_any
+
 //线程同步之条件变量
 //https://subingwen.cn/cpp/condition/
+
 //condition_variable
 //成员函数：线程等待（阻塞）函数、线程通知（唤醒）函数
 //调用wait线程被阻塞
@@ -15,4 +17,5 @@
 // void wait (unique_lock<mutex>& lck, Predicate pred);
 // 函数①：调用该函数的线程直接被阻塞
 // 函数②：该函数的第二个参数是一个判断条件，是一个返回值为布尔类型的函数
-    
+
+// 条件变量通常用于生产者和消费者模型
