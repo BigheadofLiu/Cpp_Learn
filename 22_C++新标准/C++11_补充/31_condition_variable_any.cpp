a434f03bafbc6c9cfb812ecd3919778513302f29
bigheadofliu <871314635@qq.com>
2025-04-26 14:59:37 +0800

    update

--- 22_C++新标准/C++11_补充/31_condition_variable_any.cpp
+++ 22_C++新标准/C++11_补充/31_condition_variable_any.cpp
@@ -1,24 +1,10 @@
-//
-// Created by ZYB on 2025/3/14.
-//
-// condition_variable
-// condition_variable_any
+//  condition_variable_any
+//  可以配合使用：mutex，timed_mutex,recursive_mutex,recursive_timed_mutex
+//  成员函数：wait(),wait_for(),wait_until(),均可以接收一个或两个参数
+//  notify_one()
+//  notify_all()
+//  以下改自condition_variable
 
-//线程同步之条件变量
-//https://subingwen.cn/cpp/condition/
-
-//condition_variable  只能配合 unique_lock使用
-//成员函数：线程等待（阻塞）函数、线程通知（唤醒）函数
-//调用wait线程被阻塞
-// ①
-//void wait (unique_lock<mutex>& lck);  
-// ②
-// template <class Predicate>
-// void wait (unique_lock<mutex>& lck, Predicate pred);
-// 函数①：调用该函数的线程直接被阻塞
-// 函数②：该函数的第二个参数是一个判断条件，是一个返回值为布尔类型的函数
-
-// 条件变量通常用于生产者和消费者模型
 #include<iostream>
 #include<mutex>
 #include<condition_variable>
@@ -26,20 +12,13 @@
 #include<list>
 #include<functional>
 using namespace std;
-/*  
-①
-void wait (unique_lock<mutex>& lck);
-②
-template <class Predicate>
-void wait (unique_lock<mutex>& lck, Predicate pred);
- */
 //---------------------------------
 class sync_quene{
 private:
 list<int> m_quene;
 mutex m_mutex;
-condition_variable not_empty; //   我尼玛就要用中文  消费
-condition_variable not_full;  //生产
+condition_variable_any not_empty; //   我尼玛就要用中文  消费
+condition_variable_any not_full;  //生产
 int m_maxsize;
 public:
 sync_quene(int maxsize):m_maxsize(maxsize){}
@@ -56,13 +35,14 @@ int  now_size(){
     return m_quene.size();
 }
 void list_put(const int& x){
-    unique_lock<mutex> locker(m_mutex);
+    // unique_lock<mutex> locker(m_mutex);
+        lock_guard<mutex> locker(m_mutex);
    /*  while(m_quene.size()==m_maxsize){
         cout<<"当前队列已满，niuma们还tm生产！"<<endl;
         not_full.wait(locker);  //阻塞生产
         } */
        //wait带两个参数 第二个条件（false阻塞，true继续执行）代替循环结构
-        not_full.wait(locker,[this](){
+        not_full.wait(m_mutex,[this](){
             return m_quene.size()!=m_maxsize; //不满
         });
         m_quene.push_back(x);
@@ -70,12 +50,13 @@ void list_put(const int& x){
         not_empty.notify_one(); //唤醒消费
 }
 int list_take(){
-    unique_lock<mutex> locker(m_mutex);
+    // unique_lock<mutex> locker(m_mutex);
+    lock_guard<mutex> locker(m_mutex);
    /*  while(m_quene.empty()){
         cout<<"队列空，我通知niuma去生产！"<<endl;
         not_empty.wait(locker); //阻塞消费
     } */
-    not_empty.wait(locker,[this](){
+    not_empty.wait(m_mutex,[this](){
         return !m_quene.empty(); //不空 empty true为空 false非空
     });
     int x=m_quene.front();
---
+++ 22_C++新标准/C++11_补充/out/build/gcc/CMakeFiles/Cpp_11.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zyb/Demo/CPP/VScode/Cpp_Learn/22_C++新标准/C++11_补充/30_条件变量.cpp" "CMakeFiles/Cpp_11.dir/30_条件变量.cpp.o" "gcc" "CMakeFiles/Cpp_11.dir/30_条件变量.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
