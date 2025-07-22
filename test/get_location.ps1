df0707dcc90bcf6f78de5c570e2db3bfc45b2e7a
BigheadofLiu <871314635@qq.com>
2025-07-22 17:59:51 +0800

    last update

--- test/get_location.ps1
+++
@@ -1,8 +0,0 @@
-# 查询定位服务状态
-$locationStatus = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value"
-
-if ($locationStatus.Value -eq "Allow") {
-    Write-Host "定位服务已启用。"
-} else {
-    Write-Host "定位服务未启用。"
-}
